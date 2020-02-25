6df3788ce9d49520dc20b44c58b38594114b9191
LosevaMS <pozitivka8@gmail.com>
2020-02-25 17:34:51 +0300

    Training Application

--- app/src/main/java/com/example/globusproject/DBHelper.java
+++ app/src/main/java/com/example/globusproject/DBHelper.java
@@ -3,15 +3,18 @@ package com.example.globusproject;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 public class DBHelper  extends SQLiteOpenHelper{
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "training";
 
+    public static final class ProgramEntry implements BaseColumns{
     public static final String TABLE_PROGRAMS = "programs";
     public static final String PROG_ID = "_id";
     public static final String PROG_NAME = "name";
+    }
 
     public static final String TABLE_EXERCISES = "exercises";
     public static final String EX_ID = "_id";
@@ -30,12 +33,12 @@ public class DBHelper  extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_PROGRAMS + "(" + PROG_ID
-                + " integer primary key," + PROG_NAME + " text" + ")");
+        db.execSQL("create table " + ProgramEntry.TABLE_PROGRAMS + "(" + ProgramEntry.PROG_ID
+                + " integer primary key AUTOINCREMENT," + ProgramEntry.PROG_NAME + " text" + ")");
 
         db.execSQL("create table " + TABLE_EXERCISES + "(" + EX_ID
                 + " integer primary key," + EX_NAME + " text," + EX_PROG_ID + " integer,"
-                + "foreign key("+ EX_PROG_ID+") references "+TABLE_PROGRAMS+"("+PROG_ID+")" + ")");
+                + "foreign key("+ EX_PROG_ID+") references "+ ProgramEntry.TABLE_PROGRAMS+"("+ ProgramEntry.PROG_ID+")" + ")");
 
         db.execSQL("create table " + TABLE_APPROACHES + "(" + APP_ID
                 + " integer primary key,"  + APP_WEIGHT + " integer," + APP_COUNT + " integer,"+ APP_EX_ID + " integer,"
@@ -46,7 +49,7 @@ public class DBHelper  extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists " + TABLE_APPROACHES);
         db.execSQL("drop table if exists " + TABLE_EXERCISES);
-        db.execSQL("drop table if exists " + TABLE_PROGRAMS);
+        db.execSQL("drop table if exists " + ProgramEntry.TABLE_PROGRAMS);
 
         onCreate(db);
 
--- app/src/main/java/com/example/globusproject/ListFragment.java
+++ app/src/main/java/com/example/globusproject/ListFragment.java
@@ -1,7 +1,9 @@
 package com.example.globusproject;
 
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,20 +25,29 @@ public class ListFragment extends Fragment {
 
     private Button add_program_button;
     private TextView final_text;
+    private EditText userInput;
 
+    DBHelper dbHelper;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setRetainInstance(true);
         return inflater.inflate(R.layout.fragment_list,container,false);
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setRetainInstance(true);
 
         BottomNavigationView navBar = getActivity().findViewById(R.id.nav_view);
         navBar.setVisibility(View.VISIBLE);
 
-        final NavController navController = Navigation.findNavController(view);
+        /*final NavController navController = Navigation.findNavController(view);
         TextView ex1 = view.findViewById(R.id.ex1);
         ex1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +56,18 @@ public class ListFragment extends Fragment {
 
             }
         });
-
+*/
         add_program_button = view.findViewById(R.id.add_btn);
-        final_text = view.findViewById(R.id.final_text);
+        //final_text = view.findViewById(R.id.final_text);
 
         add_program_button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
+
+                SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+                ContentValues contentValues = new ContentValues();
 
                 LayoutInflater li = LayoutInflater.from(requireContext());
                 View promptsView = li.inflate(R.layout.dialog_add_program, null);
@@ -61,7 +76,7 @@ public class ListFragment extends Fragment {
 
                 mDialogBuilder.setView(promptsView);
 
-                final EditText userInput = promptsView.findViewById(R.id.input_text);
+                userInput = promptsView.findViewById(R.id.input_text);
 
                 mDialogBuilder
                         .setCancelable(false)
@@ -79,7 +94,10 @@ public class ListFragment extends Fragment {
                                 });
                 AlertDialog alertDialog = mDialogBuilder.create();
                 alertDialog.show();
+                contentValues.put(DBHelper.ProgramEntry.PROG_NAME, final_text.toString());
+                database.insert(DBHelper.ProgramEntry.TABLE_PROGRAMS, null, contentValues);
             }
         });
+        onSaveInstanceState(savedInstanceState);
     }
 }
